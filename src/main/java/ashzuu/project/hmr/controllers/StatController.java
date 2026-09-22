ac0bfb5516710937fe0dcfb97f18ff2cdfdff026
Ashzuu <116486394+Ashzuu@users.noreply.github.com>
2026-09-22 21:44:51 +0200

    feat: adding a better clean structure for the architecture : properties not deployed, with an example file

--- src/main/java/ashzuu/project/hmr/controllers/StatController.java
+++ src/main/java/ashzuu/project/hmr/controllers/StatController.java
@@ -1,11 +1,17 @@
 package ashzuu.project.hmr.controllers;
 
 import jakarta.annotation.Resource;
+import jakarta.ws.rs.Path;
 
 /**
  * Controller to manage all stats which appears in the dashboard when the user connecting to the front.
  * The use of this controller is according to the need of users, and the profil connected.
  */
 @Resource
+@Path("/stats")
 public class StatController {
+    @Path("/global")
+    public void global(){
+        return;
+    }
 }
--- src/main/java/ashzuu/project/hmr/controllers/TrainingController.java
+++ src/main/java/ashzuu/project/hmr/controllers/TrainingController.java
@@ -1,11 +1,17 @@
 package ashzuu.project.hmr.controllers;
 
 import jakarta.annotation.Resource;
+import jakarta.ws.rs.Path;
 
 /**
  * Controller to manage all training, with authentication of not.
  * The user must be able to create new training according to known exercises, etc...
  */
 @Resource
+@Path("/training")
 public class TrainingController {
+    @Path("/all")
+    public void getAllTrainings(){
+
+    }
 }
